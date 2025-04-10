1488f0aa08ae91ad1f62e745ac10de32414c3eb0
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2025-04-10 08:45:30 -0400

    capitulo 1

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-// este es mi primer script
-/* este es mi
-segundo 
-comentario */
+// ej 1
+console.log("Manchas");
+
+// ej2
+console.log(5 + 3);
+
+// ej 3
+console.log("El resultado es: " + (5 + 3));
+
+// ej 4
 console.log(true);
+
+// ej 5
+// este es mi primer script
+
+// desafio final
+console.log("Hola me llamo, Manchas");
+console.log("mi edad es de 8 años");
+console.log("y me gusta hacer cagadas");
